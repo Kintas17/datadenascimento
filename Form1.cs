e6dbfa11d3c97760fad18c4c00e79a927a964da3
Kintas17 <49915294+Kintas17@users.noreply.github.com>
2019-11-21 12:54:09 +0000

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datadenascimento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cunha.recebe = textBox1.Text;
+            label2.Text = cunha.envia2;
+            cunha.recebe = textBox1.Text;
+            label1.Text = cunha.envia;
+            cunha.recebe = textBox1.Text;
+            label3.Text = cunha.envia3;
+        }
+        Class1 cunha = new Class1();
+        private void Label2_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
